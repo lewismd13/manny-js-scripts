a39c8b88785384620254d20f2b317f29879ad6a4
lewismd13 <72758390+lewismd13@users.noreply.github.com>
2022-01-06 15:58:35 -0600

    asdon things

--- src/testFile.ts
+++ src/testFile.ts
@@ -1,17 +1,3 @@
-import { print } from "kolmafia";
-import { SourceTerminal } from "libram";
-import { isHybridized } from "libram/dist/resources/2014/DNALab";
-import { $item, $phylum } from "libram/dist/template-string";
+import { AsdonMartin } from "libram";
 
-// /afhk; jsq
-/*
-if (stashAmount(Item.get("pantsgiving")) < 1 || stashAmount(Item.get("repaid diaper")) < 1) {
-  const page = visitUrl("clan_log.php");
-  const matcher = new RegExp(/(?:.*)(?:\d'>)(.*)(?: \(#)(?:.*)(?:took 1 Pantsgiving.)/);
-  const culprit = page.match(matcher);
-  const culpritString = culprit ? culprit[1].toString() : "";
-  print(`Nope, looks like ${culpritString} has the Pantsgiving.`, `red`);
-} else print("Good to go!", "green");
-*/
-
-SourceTerminal.extrude($item`hacked gibson`);
+AsdonMartin.drive(AsdonMartin.Driving.Observantly, 1);
